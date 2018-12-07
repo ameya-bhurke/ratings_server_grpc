bfb36037c2da8bd79759f3c8a005b5e874a78a10
Ameya Bhurke <ameyabhurke@gmail.com>
2018-12-07 17:31:59 +1100

    fixed to output grpc proto for ratings to service/ dir

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir build
+mkdir -p build/services
 cd build
-protoc -I ../services --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../services/ratings.proto
-protoc -I ../services --cpp_out=. ../services/ratings.proto
+protoc -I ../services --grpc_out=./services/. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../services/ratings.proto
+protoc -I ../services --cpp_out=./services/. ../services/ratings.proto
