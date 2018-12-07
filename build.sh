2417980b4b36e4a51c2c3a1d184354a47c249728
Ameya Bhurke <ameyabhurke@gmail.com>
2018-12-07 17:28:22 +1100

    fixed to build from the build dir

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 mkdir build
+cd build
 protoc -I ../services --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../services/ratings.proto
 protoc -I ../services --cpp_out=. ../services/ratings.proto
