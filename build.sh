e55c22cfb1ffbdf56bf187bff0815885a78eeb55
Ameya Bhurke <ameyabhurke@gmail.com>
2019-03-10 01:40:56 +1100

    set clang as compiler

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ mkdir -p build/services
 cd build
 #protoc -I ../services --grpc_out=./services/. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../services/ratings.proto
 #protoc -I ../services --cpp_out=./services/. ../services/ratings.proto
+export CXX=/usr/bin/clang++
 cmake ..
 make
